a233e0f3acaf39975236cb8aa48e674ff2ef60a1
Andrey-Patrakov <a.v.patrakov95@gmail.com>
2021-07-24 17:08:18 +0300

    123

--- z_6.py
+++
@@ -1,8 +0,0 @@
-def int_func(s):
-    return s.capitalize()
-
-s = list(map(int_func, input("Введите слова разделённые пробелом\n").split()))
-s = ' '.join(s)
-print(s)
-
-print('абв гдеёжз ийкл мнопр'.title())
