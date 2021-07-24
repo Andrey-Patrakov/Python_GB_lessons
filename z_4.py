00bb560a85e8939948ed11752961fd57cdad432c
Andrey-Patrakov <a.v.patrakov95@gmail.com>
2021-07-24 17:07:13 +0300

    Задание 3

---
+++ z_4.py
@@ -0,0 +1,36 @@
+def my_func_1(x, y):
+    try:
+        x = float(x)
+        y = int(y)
+        if x <= 0 or y >= 0:
+            raise ValueError 
+        return x**y
+    except:
+        return 'Ошибка введённых данных'
+
+def my_func_2(x, y):
+    try:
+        x = float(x)
+        y = int(y)
+        if x <= 0 or y >= 0:
+            raise ValueError 
+        
+        fract = int(f'1{"0"*len(str(x))}')
+        x = int(x * fract)
+        a = x
+        b = fract
+
+        for i in range(abs(y) - 1):
+            a *= x
+            b *= fract
+
+        return b / a
+    except:
+        return 'Ошибка введённых данных'
+
+try:
+    x, y = input('Введите действительное положительное число x и целое отрицательное число y через пробел\n').split()
+    print(my_func_1(x, y))
+    print(my_func_2(x, y))
+except:
+    print('Ошибка введённых данных')
